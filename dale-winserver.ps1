95514a1901d8e36ef284714337d465cd64271721
DavidLejeune <Lejeune.David@outlook.com>
2019-03-02 22:31:20 +0100

    first menu

--- dale-winserver.ps1
+++ dale-winserver.ps1
@@ -1,6 +1,6 @@
 # Description
 # -----------
-function show-description(){
+function show_description(){
     #feeding the narcistic beast
     Write-Host "# Description" -ForegroundColor white
     Write-Host "# -----------" -ForegroundColor white
@@ -64,8 +64,8 @@ $Menu10 = "AD Sync"
 #------------------------------------------------------------------------------
 #Functions
 #------------------------------------------------------------------------------
-function show-headerintro(){
-    Clear
+function show_headerintro(){
+    Clear-Host
     Write-Host '      ____              __        ' -ForegroundColor Yellow
     Write-Host '     / __ \   ____ _   / /      ___ ' -ForegroundColor Yellow
     Write-Host '    / / / /  / __ `/  / /      / _ \' -ForegroundColor Yellow
@@ -85,8 +85,8 @@ function show-headerintro(){
     Write-Host ' #####################################' -ForegroundColor DarkGreen
     Write-Host ''
 }
-function show-header(){
-    Clear
+function show_header(){
+    Clear-Host
     Write-Host '      ____              __        ' -ForegroundColor Yellow
     Write-Host '     / __ \   ____ _   / /      ___ ' -ForegroundColor Yellow
     Write-Host '    / / / /  / __ `/  / /      / _ \' -ForegroundColor Yellow
@@ -104,19 +104,95 @@ function show-header(){
 }
 
 # Menus
+function ask_menu(){
+        
+    #Select action
+    $Menu = Read-Host -Prompt 'Select an option ';
+    switch ($Menu)
+        {
 
+        # AD MENU -----------------------------------------------------------------------------------
+        1
+        {
+            Write-Host "`nYou have selected $(($Menu1).ToUpper())`n" -ForegroundColor DarkGreen;
+            $Menu       = $Menu1;
+            show_header;
+            ad_menu;
+            ask_menu;
+        }
+            10
+            {
+                Write-Host "`nYou have selected $(($Menu10).ToUpper())`n" -ForegroundColor DarkGreen;
+                $Menu       = $Menu10;
+                show_header;
+                adsync;
+                ask_menu;
+            }
+
+            # -----------------------------------------------------------------------------------
+            # STANDARD OPTIONS
+            666
+            {
+                Write_Host "`nYou have selected $(($Menu666).ToUpper())`n" -ForegroundColor DarkGreen;
+                $Menu       = $Menu666;
+                exit;
+                exit;
+
+            }
+
+            99
+            {
+                Write-Host "`nYou have selected $(($Menu99).ToUpper())`n" -ForegroundColor DarkGreen;
+                $Menu       = $Menu99;
+                show_header;
+                show_description;
+                ask_menu;
+            }
+
+            69
+            {
+                Write-Host "`nYou have selected $(($Menu69).ToUpper())`n" -ForegroundColor DarkGreen;
+                $Menu       = "Main Menu";
+                show_header;
+                main_menu;
+                ask_menu;
+            }
+
+
+            default {
+            Write-Host "The choice could not be determined." -ForegroundColor Red
+            }
+        }
+
+
+}
+
+function main_menu(){
+    
+    Write-Host " Menu :" -ForegroundColor Magenta;
+    Write-Host "";
+    Write-Host '    1.   '$Menu1  -ForegroundColor Gray;
+
+    standard_options;
+}
+
+function standard_options(){
+
+    Write-Host '   ';
+    Write-Host '    69.  '$Menu69 -ForegroundColor Yellow;
+    Write-Host '    99.  '$Menu99 -ForegroundColor DarkGRay;
+    Write-Host '    666. '$Menu666 -ForegroundColor Red;
+    Write-Host ""
+}
 
 
 #------------------------------------------------------------------------------
 #Start Script
 #------------------------------------------------------------------------------
-show-headerintro;
-show-description;
-sleep 5 ;
-show-header;
-
-
-#.\lib\general\show_header.ps1;
-#.\lib\menu\main.ps1;
-#.\lib\menu\ask_menu.ps1;
+show_headerintro;
+show_description;
+Start-Sleep 2 ;
+show_header;
+main_menu;
+ask_menu
 pause
